f4d6b4a29f13211c44593d32b07ff5f1340c8711
“Raghvendra <raghvendra2006@gmail.com>
2022-08-01 14:34:03 +0530

    Push TF code

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "namespace" {
+  description = "appliedaiconsulting"
+  default     = "TEST"
+  type        = string
+}
+
+variable "region" {
+  description = "Region"
+  default     = "ap-south-1"
+  type        = string
+}
